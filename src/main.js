ee0228438f227328acf6505360017b090661a232
leoneezyy <44772792+leoneezyy@users.noreply.github.com>
2019-10-07 17:29:21 +0800

    新建登录页

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
+// 导入element-ui
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 // 可以忽略，生产环境时候是否有提示
 Vue.config.productionTip = false
 
+// 注册element插件
 Vue.use(ElementUI)
 
 new Vue({
--- src/router.js
+++ src/router.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/views/Login'
 
 // 注册路由插件
 Vue.use(Router)
 
 // 创建路由对象
 export default new Router({
-    // 可忽略，默认声明
+    // 可忽略，默认声明， 主要是带#号和不带#号的区别，没有好坏之分
     mode: 'history',
 
     // 可忽略
@@ -14,6 +15,6 @@ export default new Router({
 
     // 路由的配置
     routes: [
-
+        { path: '/login', component: Login },
     ]
 })
